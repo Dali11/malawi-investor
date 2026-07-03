e15ce190e0d069eaa8f37b6d42f242f5a3199e05
Dalitso Kamphani <dalitsokamphani11@gmail.com>
2026-07-03 18:06:59 +0300

    community

---
+++ app/(public)/community/new/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ThreadPage = () => {
+  return (
+    <div>
+        <h1>We are under Development, stay Tuned...</h1>
+    </div>
+  )
+}
+
+export default ThreadPage
